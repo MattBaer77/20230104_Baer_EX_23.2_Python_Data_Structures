5b048f5c570dc8b8962b3b611d68d44f0136bdca
Matthew Baer <matt.baer77@gmail.com>
2023-01-11 02:16:04 -0500

    added skipped mode

--- 17_mode/mode.py
+++ 17_mode/mode.py
@@ -11,3 +11,11 @@ def mode(nums):
         >>> mode([2, 2, 3, 3, 2])
         2
     """
+
+    highest = nums[0]
+
+    for num in nums:
+        if nums.count(num) > highest:
+            highest = num
+
+    return highest
